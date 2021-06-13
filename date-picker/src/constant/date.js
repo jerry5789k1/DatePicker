a759864e0dba4e87ce26e972d291a1a528c2b00c
Chui Jie Qiu <angeryboy71272@gmail.com>
2021-06-13 16:11:42 +0800

    feat: Add constant for DatePicker

---
+++ date-picker/src/constant/date.js
@@ -0,0 +1,16 @@
+export const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export const DAYS = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
